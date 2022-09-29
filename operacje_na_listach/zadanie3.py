f4bf78d7db5c29f74714b92b66decfd2fafa457e
Patrycja <patrycja.mazur1997@interia.pl>
2022-09-29 17:28:35 +0200

    Funkcja do odrzucania

--- operacje_na_listach/zadanie3.py
+++ operacje_na_listach/zadanie3.py
@@ -1,7 +1,7 @@
 def reject(lista):
     parzyste=[]
     for i in lista:
-        if i%2==0:
+        if i%2==0:# jeśli reszta z dzielenia =0, liczby parzyste
             parzyste.append(i)
     return parzyste
 print(reject([1,2,3,4,5,6,7,8,]))
