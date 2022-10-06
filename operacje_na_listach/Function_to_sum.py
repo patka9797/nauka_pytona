90fc25e8f8154cdd6ea092b6b9a2281b83ac6b15
patka9797 <114613048+patka9797@users.noreply.github.com>
2022-10-06 11:24:00 +0200

    Update Function_to_sum.py

--- operacje_na_listach/Function_to_sum.py
+++ operacje_na_listach/Function_to_sum.py
@@ -1,8 +1,8 @@
 def sum(list):
-    variable= 0
+    total= 0
     for i in list:
-        variable += i
-    return variable # return sum(i for i in lista)
+        total += i
+    return total # return sum(i for i in lista)
     
     
 print(sum([1, 4, 6]))
