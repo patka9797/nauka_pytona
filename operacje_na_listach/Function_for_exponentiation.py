765ea43ee25daa297ffe4d5354d5602b7c291679
patka9797 <114613048+patka9797@users.noreply.github.com>
2022-10-06 10:48:23 +0200

    Update Function_for_exponentiation.py

--- operacje_na_listach/Function_for_exponentiation.py
+++ operacje_na_listach/Function_for_exponentiation.py
@@ -1,13 +1,13 @@
-def power(list):
+def power(list_):
     square=[]
-    for i in list:
+    for i in list_:
         square.append(i**2)#potęgowanie liczb
     return square
 print(power([2, 4, 6, 8]))
 
 def power2(list, exponent):
-    any=[]
+    any_=[]
     for i in list:
-        any.append(i**exponent)
-    return any
+        any_.append(i**exponent)
+    return any_
 print(power2([3, 4, 5, 6, 6], 3))
