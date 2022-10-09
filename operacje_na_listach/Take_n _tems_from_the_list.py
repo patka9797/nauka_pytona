9401442f4dfc000f8df1aff3372135171464a030
patka9797 <114613048+patka9797@users.noreply.github.com>
2022-10-09 14:08:38 +0200

    Update Take_n _tems_from_the_list.py

--- operacje_na_listach/Take_n _tems_from_the_list.py
+++ operacje_na_listach/Take_n _tems_from_the_list.py
@@ -2,7 +2,7 @@ def take(list_, number_of_items):
     empty=[]
     for i in list_:
         if len(empty)== number_of_items:
-            return emppty
+            return empty
         else:
             empty.append(i)
     return empty# return poza pętlą for dlatego, że jeśli lista jest mniejsza niż założona l. elementów i tak zwraca
