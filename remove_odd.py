30f8991aedc26cbbff12722b0cd41e0b59019d98
Patrycja <patrycja.mazur1997@interia.pl>
2022-10-14 10:33:44 +0200

    Function_to_remove_odd

---
+++ remove_odd.py
@@ -0,0 +1,7 @@
+def remove_odd(string):
+    string2=' '
+    for i in range(1, len(string)+1):
+        if i%2== 0:
+            string2= string2+ string[i-1]
+    return string2
+print(remove_odd('abcdefg'))
