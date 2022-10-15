6462559aea55f25a7d41cb54440a247f2a9db9d0
Patrycja <patrycja.mazur1997@interia.pl>
2022-10-15 15:41:53 +0200

    Function-to-return

---
+++ ask_the-users.py
@@ -0,0 +1,11 @@
+import string
+
+
+def ask_users():
+    string_=[]
+    
+    print(str1=-input('Podaj stringa'))
+    string_.append(str1)
+    
+    return string_
+
---
+++ return-3-signs.py
@@ -0,0 +1,9 @@
+from pickletools import string1
+
+
+def return_(string):
+
+    str1=string[0:3]
+    return str1
+
+print(return_('Python'))
